eaf51d45118cb7b5398c9e62a442aa9f65380396
waldirdev01 <waldirdev01@gmail.com>
2022-07-18 17:02:02 -0300

    Urtilizando o Consumer para especificar exatamente o que precisa ser renderizado

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -14,10 +14,39 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //TODO 03.1
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(context, listen: false); // isso faz com que o provider não reaja à mudanças
+    // apenas os widgets filhos do consumer vão reagir. Poderia colocar o Consumer envolvendo o ClipRRect, mas
+    // aí seria a mesma coisa de deixar o provider ouvindo as alterações.
+    //TODO 04: usando o parâmetro listen: false e Widget Consumer.
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: Consumer<Product>(
+            //Esse _ abaixo substitui um child que pode ser um Widget que seria passado
+            //como filho do consumer que não receberia alterações
+            builder: (context, product, _) {
+              return IconButton(
+                onPressed: () {
+                  product.toggleFavorite();
+                },
+                //depois do consumer a cor não respondeu mais ao Theme
+                icon: Icon(product.isFavorite ? Icons.favorite : Icons.favorite_border),
+                color: Theme.of(context).colorScheme.secondary,
+              );
+            }
+          ),
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.shopping_cart),
+            color: Theme.of(context).colorScheme.primary,
+          ),
+        ),
         child: GestureDetector(
           child: Image.network(
             product.imageUrl,
@@ -29,25 +58,6 @@ class ProductItem extends StatelessWidget {
               arguments: product,
             );
           },
-        ),
-        footer: GridTileBar(
-          backgroundColor: Colors.black87,
-          leading: IconButton(
-            onPressed: () {
-              product.toggleFavorite();
-            },
-            icon: Icon(product.isFavorite ? Icons.favorite : Icons.favorite_border),
-            color: Theme.of(context).colorScheme.primary,
-          ),
-          title: Text(
-            product.title,
-            textAlign: TextAlign.center,
-          ),
-          trailing: IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.shopping_cart),
-            color: Theme.of(context).colorScheme.primary,
-          ),
         ),
       ),
     );
