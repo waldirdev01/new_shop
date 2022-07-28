99aeca5a3126da756dcf3739811428b43ec2f581
waldirdev01 <waldirdev01@gmail.com>
2022-07-27 22:19:53 -0300

    Atualizando produtos

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -7,7 +7,8 @@ import 'package:new_shop/models/product.dart';
 import '../data/dummy_data.dart';
 
 class ProductList with ChangeNotifier {
-  final _url = 'https://newshopp-46acb-default-rtdb.firebaseio.com/products.json';
+  final _baseUrl =
+      'https://newshopp-46acb-default-rtdb.firebaseio.com/products';
   List<Product> _items = [];
 
   List<Product> get items => [..._items];
@@ -15,10 +16,11 @@ class ProductList with ChangeNotifier {
   int get itemsCount {
     return _items.length;
   }
+
   Future<void> loadProducts() async {
     _items.clear();
 
-    final response = await http.get(Uri.parse(_url));
+    final response = await http.get(Uri.parse('$_baseUrl.json'));
     if (response.body == 'null') return;
     Map<String, dynamic> data = jsonDecode(response.body);
     data.forEach((productId, productData) {
@@ -35,6 +37,7 @@ class ProductList with ChangeNotifier {
     });
     notifyListeners();
   }
+
   Future<void> saveProduct(Map<String, Object> data) {
     bool hasId = data['id'] != null;
 //converti para Future<void> para poder utilizar o asssinconismo para o programa
@@ -54,15 +57,22 @@ class ProductList with ChangeNotifier {
     }
   }
 
-  Future<void> updateProduct(Product product) {
+  Future<void> updateProduct(Product product) async {
     int index = _items.indexWhere((p) => p.id == product.id);
 
     if (index >= 0) {
+          await http.patch(Uri.parse('$_baseUrl/${product.id}.json'),
+              body: jsonEncode({
+                'name': product.name,
+                'description': product.description,
+                'price': product.price,
+                'imageUrl': product.imageUrl
+              }));
+
       _items[index] = product;
       notifyListeners();
     }
-    return Future.value();
-    //provisório. Ainda não entendi para que serve
+
   }
 
   void removeProduct(Product product) {
@@ -79,7 +89,7 @@ class ProductList with ChangeNotifier {
 
   Future<void> addProduct(Product product) async {
     final response = await http.post(
-      Uri.parse('$_url'),
+      Uri.parse('$_baseUrl.json'),
       body: jsonEncode(
         {
           "name": product.name,
